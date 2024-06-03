6198b6d8a217aa8064ff910cdf7045c3535c2a41
SarahAnisia <sah.anisia053@gmail.com>
2024-06-03 19:22:19 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usuário

--- EBAC.c
+++ EBAC.c
@@ -138,6 +138,7 @@ int main()
 	   printf("\t1 Registrar nomes:\n");
 	   printf("\t2 Consultar nomes:\n");
 	   printf("\t3 Deletar nomes:\n");
+	   printf("\t4 Sair do sistema.\n");
 	   printf("opçao: ");
 	
 	   scanf("%d",&opcao);//armazenando a escolha do usuario
@@ -158,6 +159,11 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigada por utilizar o sistema!");
+			return 0;
+			break;	
 			
 			default:
 			printf("Essa opçao nao esta disponivel!\n");
